0add3b6a453c1f122431e4aa9d9fd068ab4f52b3
Raghav Yadav <isitraghav@gmail.com>
2024-06-11 16:23:14 +0530

    new site update

--- src/routes/registerurl/+server.js
+++ src/routes/registerurl/+server.js
@@ -13,6 +13,9 @@ export async function POST({ request, url }) {
 			url: req.url,
 			ip: []
 		});
+		if (url.href.replace(/registerurl/g, "") = 'https://throw-me-ten.vercel.app/') {
+			return new Response('https://69x.vercel.app/' + urlrand);
+		}
 		return new Response(url.href.replace(/registerurl/g, "") + urlrand);
 	} else {
 		console.log('not valid url');
